226f31ff7c22a304063c2e0eed1591ec8fd8e1a8
benito ESTEBAN-BODIPO <besteba@c3r1s7.wethinkcode.co.za>
2018-08-27 09:34:07 +0200

    First commit project folder/file structure

---
+++ headers/ft_ls.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: besteba <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/27 09:19:08 by besteba           #+#    #+#             */
+/*   Updated: 2018/08/27 09:19:10 by besteba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_LS_H
+#define FT_LS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <dirent.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <time.h>
+# include <pwd.h>
+# include <grp.h>
+
+typedef struct s_f_info
+{
+
+} t_f_info;
+
+
+#endif
